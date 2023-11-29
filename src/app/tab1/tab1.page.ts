7b4f46b2a19eb2a20525d9bc48cd5f6d8f2b1f68
unknown <mathiasmogrovilla533@gmail.com>
2023-11-28 20:42:21 -0500

    AppDiseño

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  alertButtons = ['Aceptar'];
 
+  public alertInputs = [
+    {
+      placeholder: 'Nombre del torneo',
+    },
+    {
+      placeholder: 'Nivel',
+      attributes: {
+        maxlength: 8,
+      },
+    },
+    {
+      type: 'date',
+      placeholder: 'Fecha de inicio',
+      min: 1,
+      max: 100,
+    },
+    {
+      type: 'textarea',
+      placeholder: 'Descripcion',
+    },
   constructor() {}
 
 }
